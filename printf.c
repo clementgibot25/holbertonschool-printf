9b85c916ed9e4b4a66564f6dfcc9c14e1e099067
clementgibot25 <10500@holbertonstudents.com>
2025-03-24 12:38:12 +0100

    added the first printf function

---
+++ printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _printf - custom printf function
+ * @format: format string
+ * @...: variable arguments
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += _puts(va_arg(args, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar(*format);
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+
+	return (count);
+}
