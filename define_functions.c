6fad642b5b6e2ec643c410473b61413c8c19a1f2
clementgibot25 <10500@holbertonstudents.com>
2025-03-24 15:43:22 +0100

    fixed betty style and added man for printf

--- define_functions.c
+++ define_functions.c
@@ -12,15 +12,34 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+
+/**
+ * print_string - Prints a string to stdout.
+ * @str: The string to be printed.
+ *
+ * Return: The total number of characters printed.
+ */
 int print_string(char *str)
 {
 	int count = 0;
+
 	if (!str)
 		str = "(null)";
 	while (*str)
 		count += _putchar(*str++);
 	return (count);
 }
+
+/**
+ * print_number - Prints an integer to stdout.
+ * @n: The integer to be printed.
+ *
+ * Description: This function recursively prints the digits of an integer.
+ * If the integer is negative, a '-' character is printed before the digits.
+ * The function converts the integer to an unsigned equivalent for processing.
+ * Return: The total number of characters printed.
+ */
+
 
 int print_number(int n)
 {
